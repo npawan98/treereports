482ec109a4210befafd4fb9fdeffd6ab4232a0d1
npawan98 <npawan98@gmail.com>
2022-01-07 19:05:06 +0530

    added zones to dropdown list

--- src/json/zones.js
+++ src/json/zones.js
@@ -10,6 +10,34 @@ const zones = [
     {
         zone:'Rose Park',
         value:'Rose_Park'
+    },
+    {
+        zone:'Traingular Park',
+        value:'Traingular_Park'
+    },
+    {
+        zone:'Circular Park',
+        value:'Circular_Park'
+    },
+    {
+        zone:'Workers Park',
+        value:'Workers_Park'
+    },
+    {
+        zone:'Park 1',
+        value:'Park1'
+    },
+    {
+        zone:'Park 2',
+        value:'Park2'
+    },
+    {
+        zone:'Edward Park',
+        value:'Edward_park'
+    },
+    {
+        zone:'Victoria Park',
+        value:'Victoria_park'
     }
 ];
 export default zones;
