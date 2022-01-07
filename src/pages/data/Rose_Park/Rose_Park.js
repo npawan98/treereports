cdecde342d79c4fa3adab1822041c84a7ccf8400
npawan98 <npawan98@gmail.com>
2022-01-07 18:41:29 +0530

    changed background

--- src/pages/data/Rose_Park/Rose_Park.js
+++ src/pages/data/Rose_Park/Rose_Park.js
@@ -55,7 +55,7 @@ function Rose_Park() {
 
     return (
         <div className="Dog__Park">
-            <SubBanner Title="Generate report" subTitle="Rose Park" imgURL="https://images.unsplash.com/photo-1635886840360-a2d27a5826d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"/>
+            <SubBanner Title="Generate report" subTitle="Rose Park" imgURL="https://images.unsplash.com/photo-1605138090832-672d54ff023f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2209&q=80"/>
         
             <div className="Dog__Park-main" style={{display:'flex',placeItems:'center'}}>
             <DonutChart
