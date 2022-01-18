356cbfdcba57c2aa0fe0cbfca66a23949dd122cf
npawan98 <npawan98@gmail.com>
2022-01-18 19:14:34 +0530

    added dog park conservation

---
+++ src/Components/Common/MUI-table/dogPark/conservartionStatus/DogTable_ConservationStatus.js
@@ -0,0 +1,88 @@
+/* eslint-disable import/no-anonymous-default-export */
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+
+function DogTable_ConservationStatus() {
+
+    const [dogLeastConcern,setLeastConcern] = useState([]);
+    const[dogNotknown,setdogNotknown] = useState([]);
+    const[dogNearThreatened,setdogNearThreatened] = useState([]);
+    const[dogVulnerable,setdogVulnerable] = useState([]);
+    const[dogCriticallyEndangered,setdogCriticallyEndangered] = useState([]);
+    const[dogConservationDependent,setdogConservationDependent] = useState([]);
+
+    useEffect(()=>{
+        axios.get("https://afternoon-mountain-93761.herokuapp.com/conservationstatus/dogLeastConcern").then(res=> {
+        console.log(res.data)
+        setLeastConcern(res.data)
+        
+      })
+      axios.get("https://afternoon-mountain-93761.herokuapp.com/conservationstatus/dogNotknown").then(res=> {
+        console.log(res.data)
+        setdogNotknown(res.data)
+    })
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/conservationstatus/dogNearThreatened").then(res=> {
+        console.log(res.data)
+        setdogNearThreatened(res.data)
+    })
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/conservationstatus/dogVulnerable").then(res=> {
+        console.log(res.data)
+        setdogVulnerable(res.data)
+    })
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/conservationstatus/dogCriticallyEndangered").then(res=> {
+        console.log(res.data)
+        setdogCriticallyEndangered(res.data)
+    })
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/conservationstatus/dogConservationDependent").then(res=> {
+        console.log(res.data)
+        setdogConservationDependent(res.data)
+    })
+      },[])
+
+
+  return (
+    <TableContainer component={Paper} style={{maxWidth:'650px'}}>
+      <Table sx={{ maxWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow onClick={()=>{window.location.assign("/Dog_Park/Dog_Park-deciduous")}} style={{cursor:"pointer"}}>
+            <TableCell>Least Concern</TableCell>
+            <TableCell align="right">{dogNotknown.length}</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        <TableRow onClick={()=>{window.location.assign("/Dog_Park/Dog_Park-evergreen")}} style={{cursor:"pointer"}}>
+            <TableCell>Not known</TableCell>
+            <TableCell align="right">{dogLeastConcern.length}</TableCell>
+          </TableRow>
+          <TableRow onClick={()=>{window.location.assign("/Dog_Park/Dog_Park-endemic")}} style={{cursor:"pointer"}}>
+            <TableCell>Near Threatened </TableCell>
+            <TableCell align="right">{dogNearThreatened.length}</TableCell>
+          </TableRow>
+          <TableRow onClick={()=>{window.location.assign("/Dog_Park/Dog_Park-indigenous")}} style={{cursor:"pointer"}}>
+            <TableCell>Vulnerable</TableCell>
+            <TableCell align="right">{dogVulnerable.length}</TableCell>
+          </TableRow>
+          <TableRow onClick={()=>{window.location.assign("/Dog_Park/Dog_Park-exotic")}} style={{cursor:"pointer"}}>
+            <TableCell>Critically endangered</TableCell>
+            <TableCell align="right">{dogCriticallyEndangered.length}</TableCell>
+          </TableRow>
+          <TableRow onClick={()=>{window.location.assign("/Dog_Park/Dog_Park-endangered")}} style={{cursor:"pointer"}}>
+            <TableCell>Conservation dependent</TableCell>
+            <TableCell align="right">{dogConservationDependent.length}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default DogTable_ConservationStatus;
--- src/pages/data/Dog_Park/Dog_Park.js
+++ src/pages/data/Dog_Park/Dog_Park.js
@@ -2,118 +2,186 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom"
 import SubBanner from "../../../Components/Common/BannerImage/SubBanner/SubBanner"
+import DogTable_ConservationStatus from "../../../Components/Common/MUI-table/DogPark/conservartionStatus/DogTable_ConservationStatus";
 import DogTable from "../../../Components/Common/MUI-table/DogPark/DogTable"
 import DonutChart from "../../../Components/DashbaordComponents/Dashboard_donut/DonutChart";
 
 function Dog_Park() {
 
-    const [dogData, setDogData] = useState([]);
-    const [dogEvergreen, setDogEvergreen] = useState([]);
-    const [dogDeciduous, setDogDeciduous] = useState([]);
-    const [dogEndemic, setDogEndemic] = useState([]);
-    const [dogIndigenous, setDogIndigenous] = useState([]);
-    const [dogExotic, setDogExotic] = useState([]);
-    const [dogEndangered, setDogEndangered] = useState([]);
+  const [dogData, setDogData] = useState([]);
+  const [dogEvergreen, setDogEvergreen] = useState([]);
+  const [dogDeciduous, setDogDeciduous] = useState([]);
+  const [dogEndemic, setDogEndemic] = useState([]);
+  const [dogIndigenous, setDogIndigenous] = useState([]);
+  const [dogExotic, setDogExotic] = useState([]);
+  const [dogEndangered, setDogEndangered] = useState([]);
 
-    const arrDog = [];
+  const arrDog = [];
 
-    const [dogEvergreenPhenology, setDogEvergreenPhenology] = useState([]);
-    const [dogDeciduousPhenology, setDogDeciduousPhenology] = useState([]);
+  const [dogEvergreenPhenology, setDogEvergreenPhenology] = useState([]);
+  const [dogDeciduousPhenology, setDogDeciduousPhenology] = useState([]);
 
-    const arrDogPhenology = [];
+  const arrDogPhenology = [];
 
-    useEffect(() => {
-        axios.get("https://afternoon-mountain-93761.herokuapp.com/dogEvergreen").then((res) => {
-          console.log(res.data.length);
-          setDogEvergreen([res.data.length]);
-        });
-        axios.get("https://afternoon-mountain-93761.herokuapp.com/dogDeciduous").then((res) => {
-          console.log(res.data.length);
-          setDogDeciduous(res.data.length);
-        });
-        axios.get("https://afternoon-mountain-93761.herokuapp.com/dogEndemic").then((res) => {
-          console.log(res.data.length);
-          setDogEndemic(res.data.length);
-        });
-        axios.get("https://afternoon-mountain-93761.herokuapp.com/dogIndigenous").then((res) => {
-          console.log(res.data.length);
-          setDogIndigenous(res.data.length);
-        });
-        axios.get("https://afternoon-mountain-93761.herokuapp.com/dogExotic").then((res) => {
-          console.log(res.data.length);
-          setDogExotic(res.data.length);
-        });
-        axios.get("https://afternoon-mountain-93761.herokuapp.com/dogEndangered").then((res) => {
-          console.log(res.data.length);
-          setDogEndangered(res.data.length);
-        });
+  const [dogLeastConcern, setLeastConcern] = useState([]);
+  const [dogNotknown, setdogNotknown] = useState([]);
+  const [dogNearThreatened, setdogNearThreatened] = useState([]);
+  const [dogVulnerable, setdogVulnerable] = useState([]);
+  const [dogCriticallyEndangered, setdogCriticallyEndangered] = useState([]);
+  const [dogConservationDependent, setdogConservationDependent] = useState([]);
 
+  const arrDogConservation = [];
 
-        axios.get("https://afternoon-mountain-93761.herokuapp.com/phenology/dogEvergreen").then((res) => {
-          console.log(res.data.length);
-          setDogEvergreenPhenology([res.data.length]);
-        });
-        axios.get("https://afternoon-mountain-93761.herokuapp.com/phenology/dogDeciduous").then((res) => {
-          console.log(res.data.length);
-          setDogDeciduousPhenology(res.data.length);
-        });
-    },[]);
+  console.log("okokoko",arrDogConservation)
 
-
-    arrDog.push(
-        parseInt(dogEvergreen),
-        parseInt(dogDeciduous),
-        parseInt(dogEndemic),
-        parseInt(dogIndigenous),
-        parseInt(dogExotic),
-        parseInt(dogEndangered)
-      );
-
-      arrDogPhenology.push(
-        parseInt(dogEvergreenPhenology),
-        parseInt(dogDeciduousPhenology),
-
-      )
+  useEffect(() => {
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/dogEvergreen").then((res) => {
+      console.log(res.data.length);
+      setDogEvergreen([res.data.length]);
+    });
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/dogDeciduous").then((res) => {
+      console.log(res.data.length);
+      setDogDeciduous(res.data.length);
+    });
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/dogEndemic").then((res) => {
+      console.log(res.data.length);
+      setDogEndemic(res.data.length);
+    });
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/dogIndigenous").then((res) => {
+      console.log(res.data.length);
+      setDogIndigenous(res.data.length);
+    });
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/dogExotic").then((res) => {
+      console.log(res.data.length);
+      setDogExotic(res.data.length);
+    });
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/dogEndangered").then((res) => {
+      console.log(res.data.length);
+      setDogEndangered(res.data.length);
+    });
 
 
-    return (
-        <div className="Dog__Park">
-            <SubBanner Title="Generate report" subTitle="Dog Park" imgURL="https://images.unsplash.com/photo-1635886840360-a2d27a5826d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"/>
-        
-            <div className="Dog__Park-main" style={{display:'flex',placeItems:'center',justifyContent:"space-around"}}>
-                <DonutChart
-                title="Dog Park"
-                values={arrDog}
-                labels={[
-                "Evergreen",
-                "Deciduous",
-                "Endemic",
-                "Indigenous",
-                "Exotic",
-                "Endangered",
-                ]}
-            />
-            <DonutChart
-                title="Dog Park (Phenology)"
-                values={arrDogPhenology}
-                labels={[
-                "Evergreen",
-                "Deciduous",
-                "Endemic",
-                "Indigenous",
-                "Exotic",
-                "Endangered",
-                ]}
-            />
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/phenology/dogEvergreen").then((res) => {
+      console.log(res.data.length);
+      setDogEvergreenPhenology([res.data.length]);
+    });
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/phenology/dogDeciduous").then((res) => {
+      console.log(res.data.length);
+      setDogDeciduousPhenology(res.data.length);
+    });
 
-            </div>
-            < div style={{display: 'grid',placeItems:'center'}}>
-                <DogTable/>
-            </div>
-        
-        
+
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/conservationstatus/dogLeastConcern").then(res => {
+      console.log(res.data.length)
+      setLeastConcern(res.data.length)
+
+    })
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/conservationstatus/dogNotknown").then(res => {
+      console.log(res.data)
+      setdogNotknown(res.data.length)
+    })
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/conservationstatus/dogNearThreatened").then(res => {
+      console.log(res.data.length)
+      setdogNearThreatened(res.data.length)
+    })
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/conservationstatus/dogVulnerable").then(res => {
+      console.log(res.data.length)
+      setdogVulnerable(res.data.length)
+    })
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/conservationstatus/dogCriticallyEndangered").then(res => {
+      console.log(res.data.length)
+      setdogCriticallyEndangered(res.data.length)
+    })
+    axios.get("https://afternoon-mountain-93761.herokuapp.com/conservationstatus/dogConservationDependent").then(res => {
+      console.log(res.data.length)
+      setdogConservationDependent(res.data.length)
+    })
+  }, []);
+
+
+  arrDog.push(
+    parseInt(dogEvergreen),
+    parseInt(dogDeciduous),
+    parseInt(dogEndemic),
+    parseInt(dogIndigenous),
+    parseInt(dogExotic),
+    parseInt(dogEndangered)
+  );
+
+  arrDogPhenology.push(
+    parseInt(dogEvergreenPhenology),
+    parseInt(dogDeciduousPhenology),
+
+  )
+
+  arrDogConservation.push(
+    parseInt(dogLeastConcern),
+    parseInt(dogNotknown),
+    parseInt(dogNearThreatened),
+    parseInt(dogVulnerable),
+    parseInt(dogCriticallyEndangered),
+    parseInt(dogConservationDependent),
+  )
+
+
+  return (
+    <div className="Dog__Park">
+      <SubBanner Title="Generate report" subTitle="Dog Park" imgURL="https://images.unsplash.com/photo-1635886840360-a2d27a5826d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" />
+
+      <div className="Dog__Park-main" style={{ display: 'flex', placeItems: 'center', justifyContent: "space-around" }}>
+        <DonutChart
+          title="Dog Park"
+          values={arrDog}
+          labels={[
+            "Evergreen",
+            "Deciduous",
+            "Endemic",
+            "Indigenous",
+            "Exotic",
+            "Endangered",
+          ]}
+        />
+        <DonutChart
+          title="Dog Park (Phenology)"
+          values={arrDogPhenology}
+          labels={[
+            "Evergreen",
+            "Deciduous",
+            "Endemic",
+            "Indigenous",
+            "Exotic",
+            "Endangered",
+          ]}
+        />
+
+      </div>
+      < div style={{ display: 'grid', placeItems: 'center' }}>
+        <DogTable />
+      </div>
+
+      <div style={{ padding: "50px" }}>
+        <h2>Conservation Status</h2>
+        <div style={{ display: 'flex', margin: "50px" }}>
+          <DonutChart
+            title="Dog Park (conservation status)"
+            values={arrDogConservation}
+            labels={[
+              "Least Concern",
+              "Not known",
+              "Near Threatened",
+              "Vulnerable",
+              "Critically endangered",
+              "Conservation dependent",
+            ]}
+          />
+          <DogTable_ConservationStatus />
         </div>
-    )
+
+      </div>
+
+
+    </div>
+  )
 }
 
 export default Dog_Park
